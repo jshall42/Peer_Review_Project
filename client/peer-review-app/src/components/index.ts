fdeb8a83fd85a3abfcc0ff2337cbc6dcc9f4b252
Jake2001 <jsamh12@outlook.com>
2025-05-14 01:24:25 -0400

    Added a index file to make exports easier to read on App.tsx

---
+++ client/peer-review-app/src/components/index.ts
@@ -0,0 +1,6 @@
+// components/index.ts
+export { default as NavBar } from './NavBar'
+export { default as HomePage } from './HomePage'
+export { default as LoginPage } from './LoginPage'
+export { default as RegistrationPage } from './RegistrationPage'
+export { default as ForgotPassword } from './ForgotPassword'

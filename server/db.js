effeb3859a4f3959c353a1fddce1477e6fe346ca
Jake2001 <jsamh12@outlook.com>
2025-05-16 17:09:40 -0400

    For connection to AWS Postgres DB

---
+++ server/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    port: process.env.PG_PORT,
+    ssl: { rejectUnauthorized: false },
+})
+
+module.exports = pool
